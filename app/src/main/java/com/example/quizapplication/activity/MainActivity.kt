e92899dfe0e9239f4818b5919d57be660d296fe9
Rajeev Ranjan <rajeev@infrrdad.local>
2020-03-14 12:11:27 +0530

    edit and read mode added

--- app/src/main/java/com/example/quizapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/quizapplication/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.quizapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,19 +16,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewmodel: MainViewmodel
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: QuizAdapter
+    private lateinit var submitButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewmodel = ViewModelProvider(this).get(MainViewmodel::class.java)
         viewmodel.getQuizDataList(this)
+        submitButton = submit_button
         setupRecyclerView()
+        setupClickListener()
     }
+
     private fun setupRecyclerView(){
         recyclerView = quiz_recycler_view
-        adapter = QuizAdapter(this, viewmodel.quizDataList)
+        adapter = QuizAdapter(this, true, viewmodel.quizDataList)
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = adapter
+    }
+
+    private fun setupClickListener(){
+        submitButton.setOnClickListener {
+            Toast.makeText(this, "submit button clicked", Toast.LENGTH_SHORT).show()
+            adapter.isEditMode = false
+            adapter.notifyDataSetChanged()
+        }
     }
 }
--- app/src/main/java/com/example/quizapplication/adapter/QuizAdapter.kt
+++ app/src/main/java/com/example/quizapplication/adapter/QuizAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quizapplication.R
@@ -13,6 +14,7 @@ import com.example.quizapplication.model.QuizModel
 
 class QuizAdapter(
     val context: Context,
+    var isEditMode: Boolean,
     val dataSet: MutableList<QuizModel>
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -23,10 +25,18 @@ class QuizAdapter(
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val quizHolder = holder as QuizListViewHolder
         quizHolder.question.text = dataSet[position].question
-        quizHolder.option_one.text = dataSet[position].option1
-        quizHolder.option_two.text = dataSet[position].option2
-        quizHolder.option_three.text = dataSet[position].option3
-        quizHolder.option_fourth.text = dataSet[position].option4
+        if (isEditMode){
+            quizHolder.selected_answer.visibility = View.GONE
+            quizHolder.radioGroup.visibility = View.VISIBLE
+            quizHolder.option_one.text = dataSet[position].option1
+            quizHolder.option_two.text = dataSet[position].option2
+            quizHolder.option_three.text = dataSet[position].option3
+            quizHolder.option_fourth.text = dataSet[position].option4
+        } else {
+            quizHolder.selected_answer.visibility = View.VISIBLE
+            quizHolder.radioGroup.visibility = View.GONE
+            quizHolder.selected_answer.text = dataSet[position].selectedAnswer
+        }
 
     }
 
@@ -38,10 +48,12 @@ class QuizAdapter(
 
     inner class QuizListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val question: TextView = itemView.findViewById(R.id.row_question)
+        val radioGroup: RadioGroup = itemView.findViewById(R.id.options_radio_group)
         val option_one: RadioButton = itemView.findViewById(R.id.row_option_1)
         val option_two: RadioButton = itemView.findViewById(R.id.row_option_2)
         val option_three: RadioButton = itemView.findViewById(R.id.row_option_3)
         val option_fourth: RadioButton = itemView.findViewById(R.id.row_option_4)
+        val selected_answer: TextView = itemView.findViewById(R.id.selected_answer)
         init {
             option_one.setOnCheckedChangeListener { buttonView, isChecked ->
                 if (isChecked){
